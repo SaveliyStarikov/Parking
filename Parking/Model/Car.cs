1668b9f3a2e10ba3a794934e9248852591bca09d
Савелий <savelijstarikovapp@gmail.com>
2025-02-12 00:18:02 +0300

    Создана база данных и шаблон был переделан под ТЗ(Автостоянка)

---
+++ Parking/Model/Car.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Parking.Model
+{
+    public class Car
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Требуется номер автомобиля.")]
+        [StringLength(6, ErrorMessage = "Заголовок не может быть длиннее 6 символов.")]
+        public required string Title { get; set; }
+
+        [Required(ErrorMessage = "Требуется марка автомобиля.")]
+        [StringLength(100, ErrorMessage = "Марка автомобиля не может быть длиннее 100 символов.")]
+        public required string Author { get; set; }
+
+    }
+}
---
+++ Parking/Model/Parking_space.cs
@@ -0,0 +1,10 @@
+﻿namespace Parking.Model
+{
+    public class Parking_space
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+        public string? Email { get; set; }
+        public string? Phone { get; set; }
+    }
+}
