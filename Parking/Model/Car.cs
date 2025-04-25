7bb77dab1bc11bf34e9c3a542e6318f921061a28
Dizablee <mth78931@gmial.com>
2025-04-25 08:34:17 +0300

    Переделал валидацию данных и редактирование на странице АвтоИнфо

--- Parking/Model/Car.cs
+++ Parking/Model/Car.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
 
 namespace Parking.Model
 {
@@ -6,16 +7,19 @@ namespace Parking.Model
     {
         public int Id { get; set; }
 
+        [Display(Name = "Госномер")]
         [Required(ErrorMessage = "Требуется номер автомобиля.")]
         [StringLength(15, ErrorMessage = "Номер автомобиля не может быть длиннее 15 символов.")]
-        public string LicensePlate { get; set; }
+        public string? LicensePlate { get; set; }
 
+        [Display(Name = "Марка")]
         [Required(ErrorMessage = "Требуется марка автомобиля.")]
         [StringLength(100, ErrorMessage = "Марка автомобиля не может быть длиннее 100 символов.")]
-        public string Brand { get; set; }
+        public string? Brand { get; set; }
 
+        [Display(Name = "Модель")]
         [Required(ErrorMessage = "Требуется модель автомобиля.")]
         [StringLength(100, ErrorMessage = "Модель автомобиля не может быть длиннее 100 символов.")]
-        public string Model { get; set; }
+        public string? Model { get; set; }
     }
 }
